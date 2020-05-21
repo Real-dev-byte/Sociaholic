7a01be32d10ee55bd1143395b054163785221317
DhananjayRohmetra <35421767+DhananjayRohmetra@users.noreply.github.com>
2020-05-21 21:02:03 +0530

    make it better

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const {MONGOURI} = require('./config/key')
-const PORT=5000 || process.env.PORT
+const PORT=process.env.PORT || 5000
 
 
 mongoose.connect(MONGOURI,{
